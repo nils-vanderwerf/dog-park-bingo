54eaab474fc198e8c50e8ef78b295424e46ad772
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-08-28 15:11:42 +1000

    Added options to constant

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import {dogBingoOptions} from './constants/dogBingoOptions'
+let dimension = 4;
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pick: new Array(dimension * dimension).fill(0),
+      picked: [],
+      result: [],
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    }
+  }
+  render() {
+
+    return (
+      <div>
+        <h1>{dogBingoOptions.length}</h1>
+      </div>
+    )
+  }
 }
-
-export default App;
