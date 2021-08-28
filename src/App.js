20fa5cb719327e2a8a02ab82dcad9e4037f2da25
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-08-28 16:07:53 +1000

    Create randomizer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import {dogBingoOptions} from './constants/dogBingoOptions'
-let dimension = 4;
+import CardContainer from './components/cardContainer'
+let dimension = 5;
 export default class App extends Component {
   constructor(props) {
     super(props);
@@ -8,14 +9,47 @@ export default class App extends Component {
       pick: new Array(dimension * dimension).fill(0),
       picked: [],
       result: [],
+      row: [],
+      column: [],
+      options: dogBingoOptions,
+      hits: null
 
     }
+    this.handleGenerateRandom = this.handleGenerateRandom.bind(this)
+    this.generateCards = this.generateCards.bind(this)
+  }
+
+  handleGenerateRandom = () => {
+    console.log(this.state.options)
+    const randCards = this.generateCards(dimension * dimension)
+    console.log(randCards)
+    this.setState({
+      pick: randCards,
+      result: randCards
+    })
+  }
+
+  generateCards = (max) => {
+    //Create pool of spaces
+    const pool = Array.apply(null, Array(max+1)).map(function (_, i) {return i;});
+      let arr = [];
+      while(arr.length < max){
+        const idx = parseInt(Math.random() * (max+1));
+        if(pool[idx] != 0){
+          arr.push(pool[idx]);
+          pool[idx] = 0;
+        }
+      }
+      return arr;
   }
   render() {
-
+    
     return (
       <div>
-        <h1>{dogBingoOptions.length}</h1>
+        <h1>Bingo</h1>
+        <button>Clear</button>
+        <button onClick={this.handleGenerateRandom}>New Card</button>
+        <CardContainer/>
       </div>
     )
   }
---
+++ src/components/cardContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CardContainer extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
